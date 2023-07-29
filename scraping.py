188c95f2f996a0d9eebb0de23998c9c8bd355937
SreehariRamMohan <sreehari.rammohan@gmail.com>
2023-07-29 16:17:38 -0700

    adding the flask server

--- scraping.py
+++ scraping.py
@@ -2,12 +2,128 @@ from selenium import webdriver
 from bs4 import BeautifulSoup as bs
 from urllib.parse import urljoin
 import requests
-import os 
+import os
 from anthropic import Anthropic, HUMAN_PROMPT, AI_PROMPT
 from dotenv import load_dotenv
 import re
 import tinycss2
 import cssbeautifier
+from flask import Flask, request, jsonify
+import json
+
+app = Flask(__name__)
+
+
+@app.route('/post_endpoint', methods=['POST'])
+def post_example():
+    # Assuming the POST data is JSON, let's parse it
+    data = request.json
+
+    url = data['url']
+
+    driver = webdriver.Chrome()
+    driver.get(url)
+    page_source = driver.page_source
+
+    # parse HTML using beautiful soup
+    soup = bs(page_source, "html.parser")
+
+    # get the CSS files
+    css_files = []
+
+    for css in soup.find_all("link"):
+        if css.attrs.get("href"):
+            # if the link tag has the 'href' attribute
+            css_url = urljoin(url, css.attrs.get("href"))
+            # check if url ends in .css
+            if '.css' in css_url:
+                css_files.append(css_url)
+    
+    list_and_delete_files("./llm_input")
+
+
+    print("LENGTH before CULL is: ", len(soup.prettify('utf-8')))
+    # Tags to be removed
+    tags_to_remove = ['script', 'path', 'noscript', 'g', 'meta',
+                    'clippath', 'svg', 'link', 'br', 'source', 'video', 'img']
+
+    # Find and remove the specified tags and their contents
+    for tag_name in tags_to_remove:
+        for tag in soup.find_all(tag_name):
+            tag.decompose()  # Removes the tag from the soup
+
+    # Save the HTML to a file
+    with open('llm_input/website.html', 'wb') as file:
+        # body_tag = soup.find("body")
+        file.write(soup.prettify('utf-8'))
+
+    print("LENGTH AFTER CULL is: ", len(soup.prettify('utf-8')))
+
+    print("Total CSS files in the page:", len(css_files))
+
+    with open("css_files.txt", "w") as f:
+        for index, css_file in enumerate(css_files):
+            print(css_file, file=f)
+            if ("http" in css_file):
+                download_css_from_url(
+                    css_file, filename=f"llm_input/css_file_{index}.css")
+                    
+    HTML_CSS_STRING = read_and_append_files("./llm_input")
+
+
+    PROMPT = f"""
+
+    I am going to give you some code and I want you to extract some style values for me. 
+
+    {HTML_CSS_STRING}
+
+    I want you to extract the following style values from the code above.
+
+    KEY	                 DESCRIPTION
+    background-color     The background color of the hero section
+    text-color	         The color of the text
+    card-color	         Background color of the card elements in the page
+    card-border-radius   The border radius of the card elements in the page
+    button-color	     The color of the buttons on the page
+    button-border-radius The border radius of the buttons on the page
+
+    Please output ONLY a JSON object with the following keys and values and NOTHING more. 
+    Here is an example of the output specification. 
+
+    {{
+        background-color: #hex (example), 
+        text-color: #hex (example), 
+        card-color: #hex (example), 
+        card-border-radius: px (example),
+        button-color: #hex (example), 
+        button-border-radius: px (example),
+    }}
+
+    Do not hallucinate any values for the variables, only use values found in the code given.
+    Only output the JSON as a string. Such that we could use json.loads on your output and get a dictionary.
+
+    {{
+    """
+
+    print("querying claude 2...")
+
+    with open("input_prompt.txt", "w") as f:
+        f.write(PROMPT)
+
+    output_json = query_claude_2(f"{HUMAN_PROMPT} {PROMPT} {AI_PROMPT}")
+
+    # Extract content between { and }
+    json_match = re.search(r'\{(.*?)\}', output_json, re.DOTALL)
+
+    if json_match:
+        json_content = '{' + json_match.group(1) + '}'
+        # Parse the JSON content to get a Python dictionary
+        parsed_json = json.loads(json_content)
+        return jsonify({"output_json": parsed_json}), 404
+    else:
+        print("JSON content not found in the provided string.")
+        return jsonify({"output_json": output_json}), 404
+
 
 
 def format_css(css_content):
@@ -16,35 +132,38 @@ def format_css(css_content):
     formatted_css = cssbeautifier.beautify(css_content, opts)
     return formatted_css
 
+
 def should_keep_rule(rule, desired_properties):
     # Check if the rule is an @media rule, and if so, discard it
     if isinstance(rule, tinycss2.ast.AtRule) and rule.lower_at_keyword in ['media', 'font-face']:
         return False
 
-    # If the rule is not a QualifiedRule (doesn't have selectors and content), 
+    # If the rule is not a QualifiedRule (doesn't have selectors and content),
     # keep it by default, as it might be another type of at-rule or a comment.
     if not isinstance(rule, tinycss2.ast.QualifiedRule):
         return True
-    
+
     # Parse the declarations inside the rule
     declarations = tinycss2.parse_declaration_list(rule.content)
-    
+
     for declaration in declarations:
         # If any of the desired properties are found in the declarations, keep the rule
         if isinstance(declaration, tinycss2.ast.Declaration) and declaration.lower_name in desired_properties:
             return True
-            
+
     return False
+
 
 def filter_css_by_properties(css_content, desired_properties):
     parsed_rules = tinycss2.parse_stylesheet(css_content)
-    
+
     # Convert property names to lowercase for consistent comparisons
     desired_properties = [prop.lower() for prop in desired_properties]
-    
+
     # Filter the rules
-    cleaned_rules = [rule for rule in parsed_rules if should_keep_rule(rule, desired_properties)]
-    
+    cleaned_rules = [rule for rule in parsed_rules if should_keep_rule(
+        rule, desired_properties)]
+
     return tinycss2.serialize(cleaned_rules)
 
 
@@ -54,16 +173,16 @@ load_dotenv()
 # Access the values using os.environ
 anthropic_api_key = os.environ.get("ANTHROPIC_API_KEY")
 
-print(anthropic_api_key) 
+print(anthropic_api_key)
 
-url = "https://arc.net/"
 
 anthropic = Anthropic(
     # defaults to os.environ.get("ANTHROPIC_API_KEY")
     api_key=anthropic_api_key,
 )
 
-def query_claude_2(prompt): 
+
+def query_claude_2(prompt):
     completion = anthropic.completions.create(
         model="claude-2",
         max_tokens_to_sample=100000,
@@ -71,26 +190,6 @@ def query_claude_2(prompt):
     )
     print(completion.completion)
     return completion.completion
-
-
-driver = webdriver.Chrome()
-driver.get(url)
-page_source = driver.page_source
-
-
-# parse HTML using beautiful soup
-soup = bs(page_source, "html.parser")
-
-# get the CSS files
-css_files = []
-
-for css in soup.find_all("link"):
-    if css.attrs.get("href"):
-        # if the link tag has the 'href' attribute
-        css_url = urljoin(url, css.attrs.get("href"))
-        # check if url ends in .css
-        if '.css' in css_url:
-            css_files.append(css_url)
 
 
 def list_and_delete_files(directory_path):
@@ -117,29 +216,7 @@ def list_and_delete_files(directory_path):
             except Exception as e:
                 print(f"Error deleting {file_path}. Reason: {e}")
 
-
-list_and_delete_files("./llm_input")
-
-
-print("LENGTH before CULL is: ", len(soup.prettify('utf-8')))
-# Tags to be removed
-tags_to_remove = ['script', 'path', 'noscript', 'g', 'meta', 'clippath', 'svg', 'link', 'br', 'source', 'video', 'img']
-
-# Find and remove the specified tags and their contents
-for tag_name in tags_to_remove:
-    for tag in soup.find_all(tag_name):
-        tag.decompose()  # Removes the tag from the soup
-
-# Save the HTML to a file
-with open('llm_input/website.html', 'wb') as file:
-    # body_tag = soup.find("body")
-    file.write(soup.prettify('utf-8'))
-
-print("LENGTH AFTER CULL is: ", len(soup.prettify('utf-8')))
-
-print("Total CSS files in the page:", len(css_files))
-
-def download_css_from_url(url, filename): 
+def download_css_from_url(url, filename):
     # Sending a GET request to the URL
     response = requests.get(url)
 
@@ -150,14 +227,9 @@ def download_css_from_url(url, filename):
             file.write(response.content)
         print("CSS file downloaded successfully!")
     else:
-        print(f"Failed to download the CSS file. Status code: {response.status_code}")
+        print(
+            f"Failed to download the CSS file. Status code: {response.status_code}")
 
-
-with open("css_files.txt", "w") as f:
-    for index, css_file in enumerate(css_files):
-        print(css_file, file=f)
-        if ("http" in css_file):
-            download_css_from_url(css_file, filename=f"llm_input/css_file_{index}.css")
 
 def remove_media_selectors(css_content):
     # This regex pattern finds all @media blocks in the CSS
@@ -165,11 +237,12 @@ def remove_media_selectors(css_content):
     # Use re.sub to replace all @media blocks with an empty string
     return re.sub(pattern, '', css_content)
 
+
 def read_and_append_files(folder_path):
     # The files we want to look for
     files_to_check = ["website.html"]
-    
-    # Extend the list with possible css files. We assume there could be a large number, 
+
+    # Extend the list with possible css files. We assume there could be a large number,
     # but you can reduce the range if you have an upper limit
     for i in range(100):  # assuming a max of 100 css files, adjust as needed
         files_to_check.append(f"css_file_{i}.css")
@@ -182,7 +255,7 @@ def read_and_append_files(folder_path):
 
         # Check if the file exists
         if os.path.exists(file_path):
-            css_content = "" 
+            css_content = ""
 
             with open(file_path, 'r', encoding='utf-8') as file:
 
@@ -192,14 +265,16 @@ def read_and_append_files(folder_path):
                 cleaned_css = remove_media_selectors(css_content)
 
                 if ".css" in file_path:
-                    desired_properties = ['color', 'fontsize', 'backgroundcolor', 'borderradius']
+                    desired_properties = [
+                        'color', 'fontsize', 'backgroundcolor', 'borderradius']
                     print("LENGTH OF CSS BEFORE CLEANING is: ", len(cleaned_css))
-                    cleaned_css = filter_css_by_properties(css_content, desired_properties)
+                    cleaned_css = filter_css_by_properties(
+                        css_content, desired_properties)
                     print("LENGTH OF CSS AFTER CLEANING is: ", len(cleaned_css))
-                    
+
                     cleaned_css = format_css(cleaned_css)
 
-                    content_str +=  f"This is an additional style sheet not included in the original HTML but referenced: \n <style>{cleaned_css}</style>" + "\n"
+                    content_str += f"This is an additional style sheet not included in the original HTML but referenced: \n <style>{cleaned_css}</style>" + "\n"
                 else:
                     content_str += f"HTML: {cleaned_css}" + "\n"
 
@@ -209,46 +284,100 @@ def read_and_append_files(folder_path):
 
     return content_str
 
-HTML_CSS_STRING = read_and_append_files("./llm_input")
+
+if __name__ == "__main__":
+    # url = "https://www.anthropic.com/"
+
+    # driver = webdriver.Chrome()
+    # driver.get(url)
+    # page_source = driver.page_source
+
+    # # parse HTML using beautiful soup
+    # soup = bs(page_source, "html.parser")
+
+    # # get the CSS files
+    # css_files = []
+
+    # for css in soup.find_all("link"):
+    #     if css.attrs.get("href"):
+    #         # if the link tag has the 'href' attribute
+    #         css_url = urljoin(url, css.attrs.get("href"))
+    #         # check if url ends in .css
+    #         if '.css' in css_url:
+    #             css_files.append(css_url)
+    
+    # list_and_delete_files("./llm_input")
 
 
-PROMPT = f"""
+    # print("LENGTH before CULL is: ", len(soup.prettify('utf-8')))
+    # # Tags to be removed
+    # tags_to_remove = ['script', 'path', 'noscript', 'g', 'meta',
+    #                 'clippath', 'svg', 'link', 'br', 'source', 'video', 'img']
 
-I am going to give you some code and I want you to extract some style values for me. 
+    # # Find and remove the specified tags and their contents
+    # for tag_name in tags_to_remove:
+    #     for tag in soup.find_all(tag_name):
+    #         tag.decompose()  # Removes the tag from the soup
 
-{HTML_CSS_STRING}
+    # # Save the HTML to a file
+    # with open('llm_input/website.html', 'wb') as file:
+    #     # body_tag = soup.find("body")
+    #     file.write(soup.prettify('utf-8'))
 
-I want you to extract the following style values from the code above.
+    # print("LENGTH AFTER CULL is: ", len(soup.prettify('utf-8')))
 
-KEY	                 DESCRIPTION
-background-color     The main background color of the page
-text-color	         The color of the text
-card-color	         Background color of the card elements in the page
-card-border-radius   The border radius of the card elements in the page
-button-color	     The color of the buttons on the page
-button-border-radius The border radius of the buttons on the page
+    # print("Total CSS files in the page:", len(css_files))
 
-Please output ONLY a JSON object with the following keys and values and NOTHING more. 
-Here is an example of the output specification. 
+    # with open("css_files.txt", "w") as f:
+    #     for index, css_file in enumerate(css_files):
+    #         print(css_file, file=f)
+    #         if ("http" in css_file):
+    #             download_css_from_url(
+    #                 css_file, filename=f"llm_input/css_file_{index}.css")
+                    
+    # HTML_CSS_STRING = read_and_append_files("./llm_input")
 
-{{
-    background-color: #hex (example), 
-    text-color: #hex (example), 
-    card-color: #hex (example), 
-    card-border-radius: px (example),
-    button-color: #hex (example), 
-    button-border-radius: px (example),
-}}
 
-Do not hallucinate any values for the variables, only use values found in the code given.
-"""
+    # PROMPT = f"""
 
-print("querying claude 2...")
+    # I am going to give you some code and I want you to extract some style values for me. 
 
-with open("input_prompt.txt", "w") as f:
-    f.write(PROMPT)
+    # {HTML_CSS_STRING}
 
-output_json = query_claude_2(f"{HUMAN_PROMPT} {PROMPT} {AI_PROMPT}")
+    # I want you to extract the following style values from the code above.
 
-print("done")
-print("output json is: ", output_json)
+    # KEY	                 DESCRIPTION
+    # background-color     The background color of the hero section
+    # text-color	         The color of the text
+    # card-color	         Background color of the card elements in the page
+    # card-border-radius   The border radius of the card elements in the page
+    # button-color	     The color of the buttons on the page
+    # button-border-radius The border radius of the buttons on the page
+
+    # Please output ONLY a JSON object with the following keys and values and NOTHING more. 
+    # Here is an example of the output specification. 
+
+    # {{
+    #     background-color: #hex (example), 
+    #     text-color: #hex (example), 
+    #     card-color: #hex (example), 
+    #     card-border-radius: px (example),
+    #     button-color: #hex (example), 
+    #     button-border-radius: px (example),
+    # }}
+
+    # Do not hallucinate any values for the variables, only use values found in the code given.
+    # Only output the JSON as a string. Such that we could use json.loads on your output and get a dictionary.
+    # """
+
+    # print("querying claude 2...")
+
+    # with open("input_prompt.txt", "w") as f:
+    #     f.write(PROMPT)
+
+    # output_json = query_claude_2(f"{HUMAN_PROMPT} {PROMPT} {AI_PROMPT}")
+
+    # print("done")
+    # print("output json is: ", output_json)
+
+    app.run(debug=True)
